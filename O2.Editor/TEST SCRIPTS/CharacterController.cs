6235b769f96a09742d3441289f161881fde129af
Süleyman akbulut <132370899+OxygenButBeta@users.noreply.github.com>
2024-12-25 12:25:31 +0300

    Up

--- O2.Editor/TEST SCRIPTS/CharacterController.cs
+++ O2.Editor/TEST SCRIPTS/CharacterController.cs
@@ -5,30 +5,24 @@ using OxygenEngineCore.InputSystem;
 namespace Runtime;
 
 public class CharacterController : CoreBehaviour {
-    [SerializedField] private float speed = 1.0f;
+    /// Marked as serialized field so it can be edited in the inspector
+    [SerializedField]  float speed = 1.0f;
 
+    /// Called every frame by the engine
     internal override void OnTick(float deltaTime) {
         var nextPos = transform.Position;
-
         if (Input.IsKeyDown(Key.Up))
             nextPos.Z -= speed * deltaTime;
-
         if (Input.IsKeyDown(Key.Down))
             nextPos.Z += speed * deltaTime;
-
         if (Input.IsKeyDown(Key.Left))
             nextPos.X -= speed * deltaTime;
-
         if (Input.IsKeyDown(Key.Right))
             nextPos.X += speed * deltaTime;
-
         transform.Position = nextPos;
-
-        if (Input.IsKeyDown(Key.V))
-            SetActive(!Enabled);
     }
 
     internal override void OnActiveStateChange(bool active) {
-        Console.WriteLine("CharacterController is now " + (active ? "active" : "inactive"));
+        // Will be called when the Component is enabled or disabled
     }
 }
