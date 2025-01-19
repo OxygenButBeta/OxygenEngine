5cd7419f120449162ab55eb46b09377a2aa9aaa0
Süleyman akbulut <132370899+OxygenButBeta@users.noreply.github.com>
2025-01-19 04:58:38 +0300

    v0

---
+++ O2.Editor/Test/CC.cs
@@ -0,0 +1,31 @@
+﻿using OpenTK.Mathematics;
+using OxygenEngine.Scripting;
+using OxygenEngine.Serialization;
+using OxygenEngineCore.InputSystem;
+
+namespace OxygenEngineCore.Test;
+
+public class CC : CoreBehaviour {
+    [SerializedField] public float speed = 1;
+
+    internal override void OnTick(float deltaTime) {
+        Vector3 pos = new Vector3(0, 0, 0);
+        if (Input.IsKeyDown(Key.Up)){
+            pos.X += 1;
+        }
+
+        if (Input.IsKeyDown(Key.Down)){
+            pos.X -= 1;
+        }
+
+        if (Input.IsKeyDown(Key.Left)){
+            pos.Z += 1;
+        }
+
+        if (Input.IsKeyDown(Key.Right)){
+            pos.Z -= 1;
+        }
+
+        transform.Position += pos * deltaTime * speed;
+    }
+}
