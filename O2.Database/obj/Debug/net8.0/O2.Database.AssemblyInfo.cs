67c6d6cf942653697d77f3f479283560a84b7fd5
Süleyman akbulut <132370899+OxygenButBeta@users.noreply.github.com>
2025-08-17 17:09:27 +0300

    Device sync

--- O2.Database/obj/Debug/net8.0/O2.Database.AssemblyInfo.cs
+++ O2.Database/obj/Debug/net8.0/O2.Database.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("O2.Database")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cd7419f120449162ab55eb46b09377a2aa9aaa0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14f0f3061d1cbaa16de360a486ee7810881267ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("O2.Database")]
 [assembly: System.Reflection.AssemblyTitleAttribute("O2.Database")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
